ede8a9611d968ece2fb67d208bc62dd0ef056645
qingtian <2893308643@qq.com>
2018-02-14 21:17:35 +0800

    整合

--- app/src/main/java/com/mredrock/freshmanspecial/HomepageActivity.java
+++ app/src/main/java/com/mredrock/freshmanspecial/HomepageActivity.java
@@ -1,43 +1,44 @@
 package com.mredrock.freshmanspecial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
-public class HomepageActivity extends AppCompatActivity implements View.OnClickListener {
+import com.mredrock.freshmanspecial.cquptdata.DrawViewActivity;
+import com.mredrock.freshmanspecial.elegant.ElegantActivity;
 
-    private ImageButton mCQUPTStrategy; //邮子攻略
-    private ImageButton mCQUPTElegant; //重邮风采
-    private ImageButton mCQUPTData; //重邮数据
-    private ImageButton mCQUPTMilitaryTra; //军训特辑
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @BindView(R.id.strategy) ImageButton mCQUPTStrategy; //邮子攻略
+    @BindView(R.id.elegant) ImageButton mCQUPTElegant; //重邮风采
+    @BindView(R.id.cqupt_data) ImageButton mCQUPTData; //重邮数据
+    @BindView(R.id.military_training) ImageButton mCQUPTMilitaryTra; //军训特辑
+
+    private static final String TAG = "HomepageActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.special_2017_activity_main);
+        setContentView(R.layout.special_2017_activity_homepage);
 
-        mCQUPTStrategy = (ImageButton) findViewById(R.id.strategy);
-        mCQUPTElegant = (ImageButton) findViewById(R.id.elegant);
-        mCQUPTData = (ImageButton) findViewById(R.id.cqupt_data);
-        mCQUPTMilitaryTra = (ImageButton) findViewById(R.id.military_training);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.strategy:
-
-                break;
-            case R.id.elegant:
-
-                break;
-            case R.id.cqupt_data:
-
-                break;
-            case R.id.military_training:
-
-                break;
-        }
+    @OnClick(R.id.strategy)
+    public void launchStrategy() {
+        Intent intent = new Intent(this, DrawViewActivity.class);
+        startActivity(intent);
     }
+
+    @OnClick(R.id.elegant)
+    public void launchElegant() {
+        Intent intent = new Intent(this, ElegantActivity.class);
+        startActivity(intent);
+    }
+
 }
